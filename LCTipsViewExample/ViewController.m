550f26c4ab51fb4ba8b7423c5bcc8e1b7b57e270
Joshua Wu <battleangel@gmail.com>
2012-08-01 18:39:38 -0700

    clean up example code

--- LCTipsViewExample/ViewController.m
+++ LCTipsViewExample/ViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, retain) IBOutlet UIView *containerView;
 @property (nonatomic, retain) IBOutlet UIImageView *limeCatImage;
 @property (nonatomic, retain) IBOutlet UISearchBar *searchBar;
+@property (nonatomic, retain) IBOutlet UIButton *startButton;
 
 @end
 
@@ -21,37 +22,19 @@
 @synthesize searchBar;
 @synthesize containerView;
 @synthesize limeCatImage;
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
+@synthesize startButton;
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    LCTipsView *someTips = [[[LCTipsView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)] autorelease];
+    [someTips addTip:@"Click here to start the demo!" forView:startButton];
+    [self.view addSubview:someTips];
+    
+    [someTips showNextTip];
 }
 
 - (IBAction)showTipsAgain:(id)sender {
-    NSLog(@"view x: %f y: %f width: %f height: %f", self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height);
-    LCTipsView *someTips = [[[LCTipsView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)] autorelease];
-    someTips.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
-    
+    LCTipsView *someTips = [[[LCTipsView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)] autorelease];    
     [someTips addTip:@"Type stuff here to search" forView:searchBar];
     [someTips addTip:@"Highlight the whole container!" forView:containerView];
     [someTips addTip:@"Or just parts of it like this kitteh" forView:limeCatImage];
